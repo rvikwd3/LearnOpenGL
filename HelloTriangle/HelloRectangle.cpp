cf7d966b155fe06cafe3710ef96598412d5927c4
rvikwd3 <rvikwd3@outlook.com>
2018-10-07 23:42:05 +0530

    Rectangle color interpolation added

--- HelloTriangle/HelloRectangle.cpp
+++ HelloTriangle/HelloRectangle.cpp
@@ -76,36 +76,65 @@ int main(){
             1,  2,  3   // second triangle
     };
 
+    float colors[]{
+            1.0f,   0.0f,   0.0f,
+            0.0f,   1.0f,   0.0f,
+            0.0f,   0.0f,   1.0f,
+            1.0f,   1.0f,   1.0f
+    };
+
 	// Vertex Array Object & Vertex Buffer Object
-	// --------------------
-	unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	// ------------------------------------------
+	unsigned int points_VBO, colors_VBO, VAO, EBO;
     glGenBuffers(1, &EBO);
+
+
+	// Bind VBOs to targets GL_ARRAY_BUFFER
+	// ------------------------------------
+	glGenBuffers(1, &points_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, points_VBO);
+	glBufferData(GL_ARRAY_BUFFER,   // Copy vertex data into VBO
+				12*sizeof(float),
+				vertices,
+				GL_STATIC_DRAW
+	);
+
+    glGenBuffers(1, &colors_VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_VBO);
+	glBufferData(GL_ARRAY_BUFFER,   // Copy color data into VBO
+				12*sizeof(float),
+				colors,
+				GL_STATIC_DRAW
+	);
 
     // Bind VAO vertex array
     // ---------------------
+    glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-	// Bind VBO to target GL_ARRAY_BUFFER
-	// ---------------------_-----_------
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, points_VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glBindBuffer(GL_ARRAY_BUFFER, colors_VBO);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    // Enable Attribute arrays
+    // -----------------------
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
     // Bind EBO to target GL_ELEMENT_ARRAY_BUFFER
     // ------------------------------------------
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
-	// Copy vertex data into VBO
-	// -------------------------
-	glBufferData(GL_ARRAY_BUFFER,
-				sizeof(vertices),
-				vertices,
-				GL_STATIC_DRAW
-	);
-
     // Copy Indices data to EBO
     // ------------------------
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Safely unbind VBO as array buffer & unbind VAO (optional)
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    /*  Do not unbind the EBO */
 
 /*	Vertex Shader                                                             {{{
 *	================================================================== */
@@ -197,19 +226,10 @@ int main(){
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// Interpret VBO buffer data as vertices
-	// -------------------------------------
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-    // Safely unbind VBO as array buffer & unbind VAO (optional)
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    /*  Do not unbind the EBO */
 
     // Polygon mode
     // ------------
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
 /*	RENDER LOOP                                                               {{{
@@ -223,7 +243,7 @@ int main(){
 			// clear color buffer
 			// ------------------
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-			glClear(GL_COLOR_BUFFER_BIT);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 			// use shader program
 			// ------------------
@@ -231,6 +251,7 @@ int main(){
             glBindVertexArray(VAO);
             //glDrawArrays(GL_TRIANGLES, 0, 3);
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 			// glfw:	draw buffer and poll IO events
 			// ---------------------------------------
@@ -243,7 +264,8 @@ int main(){
     // Deallocate resources
     // --------------------
     glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &points_VBO);
+    glDeleteBuffers(1, &colors_VBO);
     glDeleteBuffers(1, &EBO);
     glfwTerminate();
 
